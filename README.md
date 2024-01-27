c7fd100097db95df9e292b402d65119649c44e98
Honzoraptor <j.ackermann@email.cz>
2024-01-27 12:30:47 +0100

    Added a README and basic tkinter ui

---
+++ README.md
@@ -0,0 +1,6 @@
+## Youtube playlist downloader
+<br />
+
+With PyTube and Tkinter.
+
+### Made by [Honzoraptor-3.1415926535](https://github.com/Honzoraptor31415)
--- playlist-downloader.py
+++ playlist-downloader.py
@@ -1,8 +1,40 @@
 from pytube import Playlist
-playlist = Playlist("https://www.youtube.com/playlist?list=PLUgpDpw4mkkuElgQuxhqCloX2Jrc0nNxb")
-print('Number of videos in playlist: %s' % len(playlist.video_urls))
+import ttkbootstrap as ttk
+import tkinter as tk
 
-for video in playlist.videos:
+window = ttk.Window(title="YouTube playlist downloader (made by Honzoraptor)", themename="darkly")
+window.geometry("1200x700")
+window.minsize(1000, 500)
+
+ctrlsPadding = 10
+
+def download():
+  for video in Playlist(inputStr.get()).videos:
+    text["text"] = f"Downloading {video.title}"
     stream = video.streams.filter(only_audio=True).first()
     print(stream.title)
     stream.download(filename=f"{video.title}.mp3", output_path="./Downloaded")
+
+main = ttk.Frame(window)
+main.pack(expand=True)
+
+heading = ttk.Label(main, text="YouTube playlist downloader", font="Calibri, 17 bold")
+heading.pack(pady=30)
+
+controlls = ttk.Frame(main)
+controlls.pack()
+
+inputStr = tk.StringVar(value="Playlist link")
+inp = ttk.Entry(controlls, textvariable=inputStr)
+inp.pack(pady=30, side="left", ipadx=ctrlsPadding, ipady=ctrlsPadding)
+
+dwnButton = ttk.Button(controlls, text="Download", cursor="hand2", command=download)
+dwnButton.pack(side="right", ipadx=ctrlsPadding, ipady=ctrlsPadding)
+
+vidInfo = ttk.Frame(main)
+vidInfo.pack()
+
+text = ttk.Label(vidInfo, font="Calibri 12", justify="center")
+text.pack()
+
+window.mainloop()
